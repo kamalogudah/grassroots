666f2da7d2106b8d69e95a998eda13b4c601762f
kamalogudah <mcpaul2058@gmail.com>
2018-07-22 19:48:48 +0300

    First template

--- hello.py
+++ hello.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return render_template('index.html')
 
 @app.route('/hello')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/about')
+def about():
+    return 'The about page'
